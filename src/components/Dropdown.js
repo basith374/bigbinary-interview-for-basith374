91c97e26d93620dc6be6020723b2c2aceac97434
Basith Kunimal <basith@intellicar.in>
2021-07-16 23:37:29 +0530

    icons

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -11,6 +11,7 @@ export default function Dropdown({ value, setValue, items }) {
         return () => document.removeEventListener('click', listener);
     }, []);
     return <div className={'ui dropdown'} onClick={() => setActive(!active)} ref={ref}>
+        <i className="filter icon" />
         <div className="text">{items[value]}</div>
         <i className="angle down icon" />
         <div className={'menu'} style={{ display: active ? 'block' : null }}>
